cb633f314e639b2e61c9a9f0136748511cd937a9
BaiyunYuan <baiyun.yuan@uzh.ch>
2022-12-18 20:09:31 +0100

    Delete homework directory

--- homework/week2/plot.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv('./data/coding-environment-exercise.csv')
-data = df.to_numpy().T
-plt.plot(data[0], data[1])
-plt.savefig('plot.png', bbox_inches='tight')
